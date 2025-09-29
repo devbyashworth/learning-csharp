4a4136b3255af6e57ef8e73dedff60c2ff809b6b
devbyashworth <ashworthsakara13@gmail.com>
2025-09-29 04:06:51 +0200

    Added c-sharp projects sln file and the HelloWorld Project

---
+++ Basics/HelloWorld/Helper.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    public static class Helper
+    {
+        public static void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye from Helper class!");
+        }
+    }
+}
---
+++ Basics/HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿// using System;
+
+// namespace HelloWorl// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello, World!");
+//         }
+//     }
+// }
+
+using HelloWorld;
+
+// Print a header
+Console.WriteLine("=== HelloWorldApp ===");
+Console.WriteLine("=== Commit-Line Args ===");
+
+// Check command-line args
+if (args.Length > 0)
+{
+    Console.WriteLine($"Hello, {args[0]}!");
+}
+else
+{
+    Console.WriteLine("Hello, World!");
+}
+
+// Call a method from another class
+Helper.SayGoodbye();
