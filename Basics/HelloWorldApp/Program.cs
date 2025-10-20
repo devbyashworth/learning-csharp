403fa4a748cca5e601adcfe601d3021d7b72978e
Ashworth <78765767+devbyashworth@users.noreply.github.com>
2025-10-20 11:00:08 +0200

    Add Hello World program that accepts command-line arguments

---
+++ Basics/HelloWorldApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Book book = new Book("LEO", "Conn Iggilden");
+Book book = new("LEO", "Conn Iggilden");
+Console.WriteLine("The book name is: " + book.name + " by " + book.author);
+
+
+class Book(string name, string author)
+{
+    public string name = name;
+    public string author = author;
+}
