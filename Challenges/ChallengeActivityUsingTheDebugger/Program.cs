eacba1d670b69a2f68c947677806f318dd944e10
Ashworth Sakara <ashworthsakara13@gmail.com>
2025-10-28 04:10:24 +0200

    Add ChallengeActivityUsingTheDebugger C# project

---
+++ Challenges/ChallengeActivityUsingTheDebugger/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Challenge Activity Using The Debugger");
+
+// Variable state challenge
+/*  
+This code instantiates a value and then calls the ChangeValue method
+to update the value. The code then prints the updated value to the console.
+*/
+int x = 5;
+
+ChangeValue(x);
+
+Console.WriteLine(x);
+
+void ChangeValue(int value)
+{
+    value = 10;
+}
+
+/*
+int x = 5;
+x = ChangeValue(x);
+Console.WriteLine(x);
+
+int ChangeValue(int value)
+{
+    value = 10;
+    return value;
+}
+*/
