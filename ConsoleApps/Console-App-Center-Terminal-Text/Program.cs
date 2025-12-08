33f5a7f8bdbffae065b7d120ce5377757f81e496
Ashworth <ashworthsakara13@gmail.com>
2025-12-08 04:19:33 +0200

    Add Console App Center Terminal Text C# project

---
+++ ConsoleApps/Console-App-Center-Terminal-Text/Program.cs
@@ -0,0 +1,17 @@
+﻿string message = "...Center Text in Termnal...";
+
+// Get console dimensions
+int windowWidth = Console.WindowWidth;
+int windowHeight = Console.WindowHeight;
+
+// Calculate horizontal and vertical positions
+int leftPadding = (windowWidth - message.Length) / 2;
+int topPadding = windowHeight / 2;
+
+// Move cursor to vertical center
+Console.SetCursorPosition(0, topPadding);
+
+// Write spaces to center horizontally, then the message
+Console.WriteLine(new string(' ', leftPadding) + message);
+
+Console.ReadKey();
